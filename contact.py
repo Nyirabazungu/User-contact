6dfed0eced42534e76289f6c512e4ac0847bc2dd
Nyirabazungu <vnyanki6@gmail.com>
2019-02-11 14:56:39 +0200

    change file name

--- contact.py
+++ contact.py
@@ -1,7 +1,2 @@
-class User:
-    contact_list=[]
-    def_init_(self,first_name,last_name,phone_number):
-        self.first_name=first_name
-        self.last_name=last_name
-        self.phone_number=phone_number
+
        
---
+++ user.py
@@ -0,0 +1,13 @@
+class User:
+    """
+    Class that generates new instances of contacts.
+    """
+    contact_list=[]#Empty contact list
+    def_init_(self,user_name,password):
+        '''
+        _init_method that helps us define properties for our objects.
+        '''
+        self.user_name=user_name
+        self.password=password
+        
+       
