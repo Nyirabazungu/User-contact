9f3e5efedd57883d706aa60ad1a68b98397cb32b
Nyirabazungu <vnyanki6@gmail.com>
2019-02-11 17:55:55 +0200

    add test for delet

--- user.py
+++ user.py
@@ -3,20 +3,53 @@ class User:
     Class that generates new instances of contacts.
     """
     user_list=[]#Empty contact list
-    def __init__(self,first_name,last_name,user_name,password):
+    # Init method up here
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
+
+    def delete_user(self):
+
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+
+        User.user_list.remove(self) 
+
+    def __init__(self,first_name,last_name,contact,user_name,email,password):
+
+        @classmethod
+    def find_by_email(cls,email): 
+     
+        '''
+        Method that takes in a email and returns a useer that matches that email.
+
+        '''
+
+        for user in cls.user_list:
+            if user.email== email:
+                return user   
         '''
         __init__ method that helps us define properties for our objects.
 
         Args:
             first_name:New user first name.
             last_name:New user last name.
+            contact:New user contact.
             user_name: New user user name.
+            user_name: New user email.
             password : New user password.
 
         ''' 
         self.first_name = first_name
-        self.last_name = last_name   
+        self.last_name = last_name  
+        self.contact= contact
         self.user_name = user_name
+        self.email = email
         self.password = password
         
        
--- user_test.py
+++ user_test.py
@@ -13,7 +13,7 @@ class TestUser(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_user = User("Nya","Vava","Nyanki","vnyanki@gmail.com") # create user object
+        self.new_user = User("Nya","Vava","0725296112","Nyanki","vnyanki@gmail.com","Kigali") # create user object
 
 
     def test_init(self):
@@ -22,8 +22,62 @@ class TestUser(unittest.TestCase):
         '''
         self.assertEqual(self.new_user.first_name,"Nya")
         self.assertEqual(self.new_user.last_name,"Vava")
+        self.assertEqual(self.new_user.contact,"0725296112")
         self.assertEqual(self.new_user.user_name,"Nyanki")
-        self.assertEqual(self.new_user.password,"vnyanki@gmail.com")
+        self.assertEqual(self.new_user.email,"vnyanki@gmail.com")
+        self.assertEqual(self.new_user.password,"Kigali")
+        
+    
+    def test_save_user(self):
+        '''
+        test_save_contact test case to test if the user object is saved into
+        the user list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.user_list),1)
+
+        
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+
+# other test cases here
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user to check if we can save multiple user
+        objects to our user_list
+        '''
+        self.new_user.save_user()
+        test_user = User("Nya","vava","0725296112","Nyanki","vnyanki@gmail.com","Kigali") # new user
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+
+    def test_delete_user(self):
+        '''
+        test_delete_user to test if we can remove a user from our user list
+        '''
+        self.new_user.save_user()
+        test_user = User("Nya","vava","0725296112","Nyanki","vnyanki@gmail.com","Kigali") # new contact
+        test_user.save_user()
+
+        self.new_user.delete_user()# Deleting a user object
+        self.assertEqual(len(User.user_list),1)   
+
+    def test_find_user_by_email(self):
+        '''
+        test to check if we can find a contact by phone number and display information
+        '''
+
+        self.new_user.save_user()
+        test_user = User("Nya","vava","0725296112","Nyanki","vnyanki@gmail.com","Kigali") # new contact
+        test_user.save_user()
+
+        found_user = User.find_by_email("vnyanki@gmail.com")
+
+        self.assertEqual(found_user.user_name,test_user.user_name)    
+ 
 
 if __name__ == '__main__':
     unittest.main()        
