6f891622f0eb57b30e2175947345a46c20d504fd
Nyirabazungu <vnyanki6@gmail.com>
2019-02-11 15:16:16 +0200

    add README.md file

--- user.py
+++ user.py
@@ -2,12 +2,10 @@ class User:
     """
     Class that generates new instances of contacts.
     """
-    contact_list=[]#Empty contact list
-    def_init_(self,user_name,password):
-        '''
-        _init_method that helps us define properties for our objects.
-        '''
-        self.user_name=user_name
-        self.password=password
+    user_list=[]#Empty contact list
+    def _init_(self,user_name,password):
+       # docstring removed for simplicity
+        self.user_name = user_name
+        self.password = password
         
        
