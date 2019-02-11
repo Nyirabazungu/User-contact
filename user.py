f01c29b2b9603b72e19eaf746e3471f465ad5793
Nyirabazungu <vnyanki6@gmail.com>
2019-02-11 16:32:54 +0200

    add first name

--- user.py
+++ user.py
@@ -3,14 +3,19 @@ class User:
     Class that generates new instances of contacts.
     """
     user_list=[]#Empty contact list
-    def _init_(self,user_name,password):
+    def __init__(self,first_name,last_name,user_name,password):
         '''
         __init__ method that helps us define properties for our objects.
 
         Args:
+            first_name:New user first name.
+            last_name:New user last name.
             user_name: New user user name.
             password : New user password.
-        '''    
+
+        ''' 
+        self.first_name = first_name
+        self.last_name = last_name   
         self.user_name = user_name
         self.password = password
         
