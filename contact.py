624a964ff396785f73cc0f655700c45624032355
Nyirabazungu <vnyanki6@gmail.com>
2019-02-11 14:34:55 +0200

    add class

---
+++ contact.py
@@ -0,0 +1,7 @@
+class Contact:
+    contact_list=[]
+    def_init_(self,first_name,last_name,phone_number,email):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.phone_number=phone_number
+        self.email=email
